26f74cef00eda9116f7d09c2e23441805eea9948
cblackburn3 <cblackburn3@huskers.unl.edu>
2025-03-28 17:10:40 -0500

    Update UnitTests.cs

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -6,7 +6,7 @@ public class Addition
     [TestMethod]
     public void Add_Valid_Blackburn()
     {
-        Assert.AreEqual(0, Program.Add("1", "2"));
+        Assert.AreEqual(3, Program.Add("1", "2"));
         Assert.AreEqual(5, Program.Add("3", "2"));
         Assert.AreEqual(12, Program.Add("5", "7"));
     }
